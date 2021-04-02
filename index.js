c409114d85aaeec76614dd55f8febd41bd8140db
ak7662 <ak7662@gmail.com>
2021-04-02 17:15:16 +0530

    second commit

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ let snake = [21,20,19]
 let squares = []
 let direction = 1
 function createGrid(){
-    //create elements
-    //create 100 of these elements
-    //add styling to these elements
-    //create array of squares
     for(let i =0 ;i<100;i++){
       const square = document .createElement("div")
       square.classList.add("square")
@@ -20,50 +16,47 @@ createGrid()
 snake.forEach(index => squares[index].classList.add("snake"))
 
 function move(){
+  if((snake[0]%10 === 0 && direction === -1) ||
+  (snake[0]%10 === 9 && direction === 1) ||
+  (snake[0] + 10 > 100 && direction === 10) || 
+  (snake[0] + 10 < 0 && direction === -10) || 
+  squares[snake[0] + direction].classList.contains('snake')){
+    return clearInterval(timerId)
+  }
   const tail = snake.pop()
   squares[tail].classList.remove("snake")
   snake.unshift(snake[0]+direction)
   console.log(snake)
   squares[snake[0]].classList.add("snake")
-  // snake.forEach(index => squeres[index].classList.remove("snake"))
-  // snake = snake.map(mapValue => mapValue+1)
-  // console.log(snake)
-  // snake.forEach(index => squeres[index].classList.add("snake"))
-
 }
-// let flag = true
-// move()
-// while(flag){
-//   move()
-// }
-// startButton.addEventListener("click", move) 
-// const timerId = setInterval(move, 1000)
+let timerId = setInterval(move, 1000)
+startButton.addEventListener("click", function(){
+  console.log(timerId)
+  if(timerId){
+    clearInterval(timerId)
+    timerId = 0
+  }
+  else{
+    timerId = setInterval(move, 1000)
+  }
+})
 function control(e){
-  // e.keyCode is depricated now use e.key 
-  // if(e.keyCode === 37){
-  //   console.log("left key")
-  // }
-  // else if(e.keyCode === 38){
-  //   console.log("up key")
-  // }
-  // else if(e.keyCode === 39){
-  //   console.log("right key")
-  // }
-  // else if(e.keyCode === 40){
-  //   console.log("down key")
-  // }
   switch(e.keyCode) {
     case 40:
     console.log('pressed down')
+    direction = 10
     break
     case 38:
     console.log('pressed up')
+    direction = -10
     break
     case 37: 
     console.log('pressed left')
+    direction = -1
     break
     case 39:
     console.log('pressed right')
+    direction = 1
     break
 }
   
